97780db42820e76c8bd6ca68ece16909a68b19df
Brendon Ho <bho@Brendons-MacBook-Pro.local>
2017-09-23 13:24:44 -0400

    h

--- CalARie/ViewController.swift
+++ CalARie/ViewController.swift
@@ -121,14 +121,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                     print("Malformed node name")
                     continue
                 }
-<<<<<<< HEAD
+
                 
-                FoodManager.shared().addPillHistory(foodName: components[0], calories: Int(components[1])!)
-=======
                 var nutritionFacts = JSON(components[1])
                 let cal = Int(nutritionFacts["Energy"].string!)
                 FoodManager.shared().addPillHistory(foodName: components[0], calories:cal!, nutrition: nutritionFacts)
->>>>>>> 6a384f98c03d367840e71756bce863ebb39986c1
+
                 return
             }
             if nutrition.contains(result.node) {
@@ -292,7 +290,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                     }
                     
                     let buttonNode = self.createButton(size: CGSize(width: imageView.frame.width - 128, height: 84))
-                    var b = JSON(dictionary).string
+                    var b = JSON([dictionary]).rawString()
                     buttonNode.name = result.itemName + "C==3\(b)"
                     self.buttons.append(buttonNode)
                     
