b12c9ae1d86f8de0f476fb440e098e9812075771
Brendon Ho <bho@Brendons-MacBook-Pro.local>
2017-09-23 11:33:17 -0400

    asyy

---
+++ CalARie/HistoryTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewCell.swift
+//  CalARie
+//
+//  Created by Brendon Ho on 9/23/17.
+//  Copyright © 2017 Brendon Ho. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell:UITableViewCell{
+    
+}
